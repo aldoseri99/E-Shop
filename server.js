b9ced20a17006eb8e46bdefa10bbb1451e288ac9
jaffer-Sh <jaffer121405sh@gmail.com>
2024-09-29 12:39:01 +0300

    items index

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
-
+const expressLayouts = require('express-ejs-layouts')
 //PORT Configurations
 
 const PORT = process.env.PORT
@@ -8,6 +8,8 @@ const PORT = process.env.PORT
 //intitialize express
 
 const app = express()
+
+app.use(expressLayouts)
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`)
