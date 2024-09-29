0d8e4a89591430e3a0be3a206a8cf0dd223f84c3
saraisa12 <saraisa.024@gmail.com>
2024-09-29 14:02:29 +0300

    done with item crud

--- controllers/item.js
+++ controllers/item.js
@@ -15,11 +15,41 @@ exports.item_add_post = (req, res) => {
   console.log(req.body)
   let item = new Item(req.body)
 
-  Category.findById(req.body.category)
-    .then((category) => {
-      category.item.push(item)
-      category.save()
+  item
+    .save()
+    .then(() => {
+      Category.findById(req.body.category)
+        .then((category) => {
+          category.item.push(item)
+          category.save()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
 
+      res.redirect("/item/index")
+    })
+
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+exports.item_index_get = (req, res) => {
+  Item.find()
+    .populate("category")
+    .then((items) => {
+      res.render("item/index", { items })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+exports.item_delete_get = (req, res) => {
+  console.log(req.query.id)
+  Item.findByIdAndDelete(req.query.id)
+    .then(() => {
       res.redirect("/item/index")
     })
     .catch((err) => {
@@ -27,45 +57,26 @@ exports.item_add_post = (req, res) => {
     })
 }
 
-/*
-exports.category_index_get = (req, res) => {
-  Category.find()
-    .then((categories) => {
-      res.render("category/index", { categories })
+exports.item_edit_get = (req, res) => {
+  console.log(req.query.id)
+  Item.findById(req.query.id)
+    .then((item) => {
+      return Category.find().then((categories) => {
+        res.render("item/edit", { item, categories })
+      })
     })
+
     .catch((err) => {
       console.log(err)
     })
 }
 
-exports.category_delete_get = (req, res) => {
-  Category.findByIdAndDelete(req.query.id)
-
+exports.item_update_post = (req, res) => {
+  Item.findByIdAndUpdate(req.body.id, req.body)
     .then(() => {
-      res.redirect("/category/index")
+      res.redirect("/item/index")
     })
     .catch((err) => {
       console.log(err)
     })
 }
-
-exports.category_edit_get = (req, res) => {
-  Category.findById(req.query.id)
-    .then((category) => {
-      res.render("category/edit", { category })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
-exports.category_update_post = (req, res) => {
-  Category.findByIdAndUpdate(req.body.id, req.body)
-    .then(() => {
-      res.redirect("/category/index")
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-*/
--- routes/item.js
+++ routes/item.js
@@ -13,5 +13,9 @@ const itemCntrl = require("../controllers/item")
 
 router.get("/add", itemCntrl.item_add_get)
 router.post("/add", itemCntrl.item_add_post)
+router.get("/index", itemCntrl.item_index_get)
+router.get("/delete", itemCntrl.item_delete_get)
+router.get("/edit", itemCntrl.item_edit_get)
+router.post("/update", itemCntrl.item_update_post)
 
 module.exports = router
