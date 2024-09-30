361ad4389fa361fdf8145187d473b6ea964e8b13
aldoseri99 <85260159+aldoseri99@users.noreply.github.com>
2024-09-30 13:39:58 +0300

    Update item.js

--- routes/item.js
+++ routes/item.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
 //use parser
@@ -9,13 +9,14 @@ router.use(
   })
 )
 
-const itemCntrl = require('../controllers/item')
+const itemCntrl = require("../controllers/item")
 
-router.get('/add', itemCntrl.item_add_get)
-router.post('/add', itemCntrl.item_add_post)
-router.get('/index', itemCntrl.item_index_get)
-router.get('/delete', itemCntrl.item_delete_get)
-router.get('/edit', itemCntrl.item_edit_get)
-router.post('/update', itemCntrl.item_update_post)
+router.get("/add", itemCntrl.item_add_get)
+router.post("/add", itemCntrl.item_add_post)
+router.get("/index", itemCntrl.item_index_get)
+router.get("/delete", itemCntrl.item_delete_get)
+router.get("/edit", itemCntrl.item_edit_get)
+router.post("/update", itemCntrl.item_update_post)
+
 
 module.exports = router
